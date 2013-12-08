a2a6ac7eb9ae99ac9086bf088b5a12be27266b58
itzikrb <itzikrb@walla.com>
2013-12-08 10:42:38 +0200

    strat test

---
+++ Stringcal/src/cal/StrCal.java
@@ -0,0 +1,24 @@
+package cal;
+
+public class StrCal 
+{
+   
+	public int add(String str)
+	 {
+	   /*	
+		int i;
+		for(i=0;i<str.length();i++)
+		{
+			if(CharA)
+			
+		      System.out.println(temp);
+		}
+		int sum=0;
+		return sum;
+		*/
+		return;
+	 }
+
+	
+	
+}
---
+++ Stringcal/src/cal/str.java
@@ -0,0 +1,25 @@
+package cal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class str
+{
+	@Test
+	public void  tester()
+	 {
+		 StrCal temp=new  StrCal();
+		 
+		 temp.add(" ");
+	 }
+
+	/*
+	@Test	
+	public void  tester()
+	 {
+		// StrCal temp=new  StrCal();
+		 
+	 }
+	*/ 
+}
